55491cac5da4403458f95ea2534a50f6016df9d2
Yuncheng Li <raingomm@gmail.com>
2017-03-23 11:36:39 -0700

    make vgg_16a mimic ssd

--- slim/nets/vgg.py
+++ slim/nets/vgg.py
@@ -59,7 +59,8 @@ def vgg_arg_scope(weight_decay=0.0005):
                       activation_fn=tf.nn.relu,
                       weights_regularizer=slim.l2_regularizer(weight_decay),
                       biases_initializer=tf.zeros_initializer()):
-    with slim.arg_scope([slim.conv2d], padding='SAME') as arg_sc:
+    with slim.arg_scope([slim.conv2d,
+      slim.max_pool2d], padding='SAME') as arg_sc:
       return arg_sc
 
 
@@ -103,7 +104,7 @@ def vgg_a(inputs,
       net = slim.repeat(net, 2, slim.conv2d, 512, [3, 3], scope='conv5')
       net = slim.max_pool2d(net, [2, 2], scope='pool5')
       # Use conv2d instead of fully_connected layers.
-      net = slim.conv2d(net, 1024, [3, 3], rate=3, padding='VALID', scope='fc6')
+      net = slim.conv2d(net, 1024, [3, 3], scope='fc6')
       net = slim.dropout(net, dropout_keep_prob, is_training=is_training,
                          scope='dropout6')
       net = slim.conv2d(net, 1024, [1, 1], scope='fc7')
@@ -217,9 +218,9 @@ def vgg_16a(inputs,
         net = slim.repeat(net, 3, slim.conv2d, 512, [3, 3], scope='conv4')
         net = slim.max_pool2d(net, [2, 2], scope='pool4')
         net = slim.repeat(net, 3, slim.conv2d, 512, [3, 3], scope='conv5')
-        net = slim.max_pool2d(net, [2, 2], scope='pool5')
+        net = slim.max_pool2d(net, [2, 2], stride=1, scope='pool5')
         # Use conv2d instead of fully_connected layers.
-        net = slim.conv2d(net, 1024, [3, 3], rate=3, padding='VALID', scope='fc6')
+        net = slim.conv2d(net, 1024, [3, 3], rate=6, scope='fc6')
         net = slim.dropout(net, dropout_keep_prob, is_training=is_training,
                            scope='dropout6')
         net = slim.conv2d(net, 1024, [1, 1], scope='fc7')
